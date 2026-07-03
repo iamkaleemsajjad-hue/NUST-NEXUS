d291d59767d987a2d73e49ebe7b20e3eea4ec5a7
Kaleem Sajjad <iamkaleemsajjad@gmail.com>
2026-07-03 18:45:25 +0500

    Replace Scholar Nexus logo with favicon SVG across the application

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,11 +1,11 @@
 /**
  * SCHOLAR NEXUS Logo
  * Returns an img tag referencing the official Scholar Nexus logo SVG
- * The new logo file is located at /Scholar Nexus logo.svg in public/
+ * The new logo file is located at /favicon.svg in public/
  */
 export function getLogoSVG(className = "", width = "36", height = "36") {
   return `<img
-    src="/Scholar Nexus logo.svg"
+    src="/favicon.svg"
     class="${className}"
     width="${width}"
     height="${height}"
